8e1bb4e2c615befb0ab2a4fc3e7fb7f51944718e
aasif112 <engr.aasifali@gmail.com>
2024-08-13 00:48:03 +0500

    changes in self_practice

--- src/App.js
+++ src/App.js
@@ -3,24 +3,38 @@
 
 import React, { useState } from 'react';
 import './App.css';
+import { NumChecker } from './RandomNum';
+import { Counter } from './RandomNum';
 
 function App() {
-  let RandomNum = parseInt(Math.random()*100 +1)
+  let [RandomNum, setRandomNum] = useState() 
   let [isMorning, setMorning] = useState(true)
+  let [evenCount, setevenCount] = useState(0)
+  let [oddCount, setoddCount] = useState(0)
   return (
     <div>
     <div >
       <br />
-      <h3>Random Number: {RandomNum} </h3>
+      <h2>Random Number: {RandomNum} </h2>
+      <br />
+      <button onClick={()=> {setRandomNum(parseInt(Math.random()*100 +1))
+        {RandomNum%2 === 0 ? setevenCount(++evenCount) : setoddCount(++oddCount)}
+      }}>Generate Number</button>
+      <br />
+      <NumChecker numType= {RandomNum%2 === 0 ? 'Even' : 'Odd'} getClass= {RandomNum%2 === 0 ? 'even' : 'odd'} />
+      <br />
       <br />
       
-       
+      <Counter evenCounts = {evenCount}  oddCounts = {oddCount} evenClass= {RandomNum%2 === 0 ? 'highlight' : 'simple'}
+      oddClass = {RandomNum%2 === 0 ? 'simple'  : 'highlight'} />
+      
+
     </div>
 
-    <div className= {`${isMorning? 'sunRise' : 'box'}`}>
+    <div className= { `${isMorning? 'sunRise' : 'box'}` }>
       <h4>Setting Time-Zone</h4>
       <br />
-      <h1>Good {isMorning? "Morning" : "Night"}!</h1>
+      <h1>Good {  isMorning? "Morning" : "Night"  }!</h1>
       <br />
     <button id='timeBtn' onClick={()=> setMorning(!isMorning)}>Change Time</button>
 
--- src/RandomNum.js
+++ src/RandomNum.js
@@ -3,10 +3,18 @@
 
 import React from "react";
 
-function Ramd() {
+export function NumChecker({numType, getClass}) {
         return (
-            
+                <h2>&emsp;The Number is : &emsp; <span className={getClass} > {numType}</span> </h2>
         )
 }
 
+export function Counter(props) {
+        return(
+        <div>
+                <h3 className={props.evenClass} >Even Number Appeared: &emsp; {props.evenCounts} </h3>
+                <h3 className={props.oddClass}>Odd Number Appeared: &emsp; {props.oddCounts} </h3>
+        </div>
+        )
+}
 
