2dac6eb9b99d6894d4459085f10937c35544f484
aasif112 <engr.aasifali@gmail.com>
2024-08-16 02:52:54 +0500

    Odd_Even Number counter- stage01 | To-Set counting Logic

--- src/App.js
+++ src/App.js
@@ -1,47 +1,16 @@
 
 
 
-import React, { useState } from 'react';
-import './App.css';
-import { NumChecker } from './RandomNum';
-import { Counter } from './RandomNum';
+
+import { NumGenerator } from './RandomNum';
 
 function App() {
-  let [RandomNum, setRandomNum] = useState() 
-  let [isMorning, setMorning] = useState(true)
-  let [evenCount, setevenCount] = useState(0)
-  let [oddCount, setoddCount] = useState(0)
   return (
     <div>
-    <div >
-      <br />
-      <h2>Random Number: {RandomNum} </h2>
-      <br />
-      <button onClick={()=> {setRandomNum(parseInt(Math.random()*100 +1))
-        {RandomNum%2 === 0 ? setevenCount(++evenCount) : setoddCount(++oddCount)}
-      }}>Generate Number</button>
-      <br />
-      <NumChecker numType= {RandomNum%2 === 0 ? 'Even' : 'Odd'} getClass= {RandomNum%2 === 0 ? 'even' : 'odd'} />
-      <br />
-      <br />
-      
-      <Counter evenCounts = {evenCount}  oddCounts = {oddCount} evenClass= {RandomNum%2 === 0 ? 'highlight' : 'simple'}
-      oddClass = {RandomNum%2 === 0 ? 'simple'  : 'highlight'} />
-      
-
+      <NumGenerator />
     </div>
-
-    <div className= { `${isMorning? 'sunRise' : 'box'}` }>
-      <h4>Setting Time-Zone</h4>
-      <br />
-      <h1>Good {  isMorning? "Morning" : "Night"  }!</h1>
-      <br />
-    <button id='timeBtn' onClick={()=> setMorning(!isMorning)}>Change Time</button>
-
-    </div>
-    </div>
-    
-  );
+  )
+  
 }
 
 export default App;
--- src/RandomNum.js
+++ src/RandomNum.js
@@ -1,20 +1,67 @@
 
 
 
-import React from "react";
+import React, { useState } from 'react';
+import './App.css';
 
-export function NumChecker({numType, getClass}) {
-        return (
-                <h2>&emsp;The Number is : &emsp; <span className={getClass} > {numType}</span> </h2>
-        )
-}
 
-export function Counter(props) {
-        return(
+export function NumGenerator() {
+        let [RandomNum, setRandomNum] = useState()
+        let [evenCount, setevenCount] = useState(0)
+        let [oddCount, setoddCount] = useState(0)
+        function Start() {
+                console.log({RandomNum})
+                setRandomNum(parseInt(Math.random()*100 +1))
+                if(RandomNum%2 ===0) {
+                        
+                        console.log('Even')
+                        setevenCount(++evenCount)
+                } else if(RandomNum%2 ===1) {
+                        setoddCount(++oddCount)
+                        console.log('Odd')
+                }
+        }
+
+
+  return (
+   <div>
+      {/* NUMBER GENEARTOR */}
+      <div>
+        <br />
+           <h2>Random Number: {RandomNum} </h2>
+           <br />
+           <button onClick={Start}>  
+              Generate Number
+           </button>
+      </div>
+        
+        <br />
+        {/* ODD-EVEN CHECKING */}
+           <h2>&emsp;The Number is : &emsp; 
+                        <span className={RandomNum%2===0? 'even' : 'odd'} > {RandomNum%2===0? 'Even' : 'Odd'}
+                        </span> 
+           </h2>
+        <br />
+        {/* ODD EVEN COUNTER */}
         <div>
-                <h3 className={props.evenClass} >Even Number Appeared: &emsp; {props.evenCounts} </h3>
-                <h3 className={props.oddClass}>Odd Number Appeared: &emsp; {props.oddCounts} </h3>
+           <h3 className={RandomNum%2===0? 'highlight' : 'simple'} >
+                Even Number Appeared: &emsp; {evenCount} 
+           </h3>
+           <h3 className={RandomNum%2===0? 'simple' : 'highlight'}>
+                Odd Number Appeared: &emsp; {oddCount} 
+           </h3>
         </div>
-        )
+
+   </div>
+)
 }
+ 
+
+
+
+
+
+
+
+
 
